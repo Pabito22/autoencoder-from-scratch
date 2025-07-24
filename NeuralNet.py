7681aa4778dcdf2c897d7880dfee76f516962ba1
Pawel <witoszap@gmail.com>
2025-07-24 12:38:01 +0200

    Sigma values updated.

--- NeuralNet.py
+++ NeuralNet.py
@@ -47,6 +47,7 @@ class Layer:
         Initialise the Layer. 
         input_shape - the shape of the input that is fed into the NN
         nr_neurons - nr of neurons (Perceptros) in the NN
+        First (bolean): is this Layer first in NN?
         """
         layer_neurons_list = []
         for _ in range(nr_neurons):
@@ -80,6 +81,8 @@ class Layer:
         """
         Produce the output of the NN's layer and store the result.
         """
+        self.neuron_nets = []
+
         results = np.zeros(self.nr_neurons)
         for i in range(self.nr_neurons):
             results[i] = self.layer_neurons_list[i].predict(layer_input)
@@ -97,13 +100,18 @@ class NN:
         """
         Initializes the NN
 
+        activation: contains activations of every layer, including the input
+
         nn_input_size: defines the input size fot the first layer
+        For the first layer you create 'input-layer -' !! Not only input! 
         """
         self.nn_input_size = nn_input_size
         #list for storing the layers
         self.nn_layer_list = []
         #counts how many layers there are currently
         self.layer_number = 0 #SET TO PRVATE LATER!!
+        #for storing the activations + input (gradient calculation)
+        self._a = []
     
     def add_layer(self, nr_neurons, activation_funct = relu):
         """
@@ -119,9 +127,9 @@ class NN:
             self.nn_layer_list.append(Layer(input_shape=next_layer_input_size,
                                             nr_neurons=nr_neurons,activation_function=activation_funct))
         else:
-            #save the output size of the last layer and put as input to the new layer
-            next_layer_input_size = self.nn_input_size
-            self.nn_layer_list.append(Layer(input_shape=next_layer_input_size,
+            #First layer
+            first_layer_input_size = self.nn_input_size
+            self.nn_layer_list.append(Layer(input_shape=first_layer_input_size,
                                             nr_neurons=nr_neurons,activation_function=activation_funct))
 
         #increment the value of layers stored in NN
@@ -132,15 +140,21 @@ class NN:
         Gives the output from the NN.
         Params:
         nn_input: the input(e.g. image) fed into the nn
+
+        Resets neuron_nets and _a!!!
         """
+        self.neuron_nets = [] 
+        self._a = [] 
+
+        self._a.append(nn_input)
         #calculate the result for the first layer and save it
-        self.nn_layer_list[0].predict(nn_input)
+        self._a.append(self.nn_layer_list[0].predict(nn_input))
         #update the rest of nn'a layesrs
         for i in range(1,self.layer_number):
             #get the output from last layer (input to the next one)
-            output_from_last = self.nn_layer_list[i-1].layer_output
+            output_from_last = self._a[i]
             #predict on next layer
-            self.nn_layer_list[i].predict(output_from_last)
+            self._a.append(self.nn_layer_list[i].predict(output_from_last))
 
         return self.nn_layer_list[-1].layer_output
 
@@ -160,7 +174,7 @@ class NN:
         exp_output : Expected result of neural network.
         Returns:
         sigma_arr: Array containing sigma vals for every node in evry layer
-        [l1 sigma val, l2sigma val, ...., ln sigma val]
+        np.array([1 sigma val, 2 sigma val, ...., L sigma val])
         """
         sigma_arr = []
         
@@ -192,6 +206,9 @@ class NN:
             sigma = f_prim * (W.T @ sig_old)
             sigma_arr.append(sigma)
             sig_i+=1
+        
+        #make the sigma in the right order
+        sigma_arr = sigma_arr[::-1]
         return sigma_arr
         
     def backward_pass(self, exp_output, lbd=0.1):
@@ -200,21 +217,20 @@ class NN:
         Use it after calling the self.predict method.
 
         exp_output (np.array) : expectef output for a training sample
+        nn_input (np.array): input that was fed into the neural net !MUST BE THE SAME AS THE ONE USED IN PREDICT METHOD!)
         """
         if self.layer_number < 2:
             raise ValueError("Cannot do backpropagation with only one layer.")
 
         #get the sigma values for every layer
         last_layer = self.nn_layer_list[-1]
-        last_layer_output = last_layer.layer_output
         sigma_arr = self._get_sigma_values(exp_output)
-        
-        sigma = sigma_arr[0]
+        sigma = sigma_arr[-1]
         #the outputs from the layer previous to the last one
         prev_layer_output = self.nn_layer_list[-2].layer_output
 
         #LIST for storing new weights values for all neurons
-        new_weights_list = []
+        new_weights_of_last_layer = []
         
         #get the derivatives for the weights for every neuron in the lasy layer
         for i in range(last_layer.nr_neurons):
@@ -232,12 +248,22 @@ class NN:
             print(f"gradient: {grad_i} + del b = {sigma_i} ")
             print(f"New weights + bias: {new_weights} b_new = {new_bias}")
             #save the new weights
-            new_weights_list.append(np.concat([new_weights, [new_bias]]))
+            new_weights_of_last_layer.append(np.concatenate([new_weights, [new_bias]]))
 
-        #NOW HIDDEN LAYERS
-        #for layer in self.nn_layer_list[:-1]:
-            #FIRSTLY U NEED SIGMA!!
 
+        
+        #NOW HIDDEN LAYERS + Last Layer for comparasion
+        #array for storing gradients with respect to every layer
+        gradients_w = [] #first element contains gradient for last weights (in the right)
+       
+        print("sigma_arr")
+        print(sigma_arr)
+
+
+
+
+      
+            
         return ("Ala")
 
     
