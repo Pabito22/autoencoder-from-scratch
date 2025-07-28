d9f1e4687f8b622b607d68f715df1b460a289191
Pawel <witoszap@gmail.com>
2025-07-28 17:53:11 +0200

    Perceptron and Layer improvement.

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def relu(arr, derivative=False):
+def leaky_relu(arr, derivative=False):
     if derivative:
         return np.where(arr > 0, 1, 0.01)
     return np.where(arr > 0, arr, arr * 0.01)
@@ -11,13 +11,14 @@ class Perceptron:
     A class that is supposed to simulate a single node in NN.
     """
 
-    def __init__(self, inpt_shape, activation_fun=relu):
+    def __init__(self, inpt_shape: int, activation_fun=leaky_relu):
         """
-        This function is supposed just to create the Perceptron!
-        Initializes the weights and adds the score function.
-        
-        input_shape: the size of the stuff that is fed into the neuron it has to be 1D array
-        score_function: one of the posiible score functions
+        Initialise the Perceptron:
+        - Initializes weights from N(0, 1), including bias.
+        - Sets the activation function.
+
+        inpt_shape: int, the number of input features (excluding bias)
+        activation_fun: callable, the activation function (e.g., ReLU or sigmoid)
         """
         #initialise the weights randomly [weight + bias]
         self.weights = np.random.normal(size=inpt_shape+1) #+1 to account for bias!
@@ -27,11 +28,15 @@ class Perceptron:
     def predict(self, perc_input):
       """
       Outputs the Result of the Perceptron.
-      perc_input the input that is fed into the neuron.
+      perc_input: the input that is fed into the neuron.
       Result:
       The output of perceptron.
       """  
-      net_perc = np.sum(self.weights[:-1] * perc_input) + self.weights[-1]
+      #Check if inputs shapes agree
+      assert perc_input.shape[0] == self.weights.shape[0] - 1, \
+        f"Expected input of shape {(self.weights.shape[0] - 1,)}"
+
+      net_perc = np.dot(self.weights[:-1], perc_input) + self.weights[-1]
       self.net_perc = net_perc
       self.perc_output = self.activation_fun(net_perc)
       return self.perc_output
@@ -42,51 +47,57 @@ class Layer:
     Represents the single layer of NN.
     neuron_nets: the list of net sums of evry perceptron in the layer
     """
-    def __init__(self,input_shape, nr_neurons, activation_function=relu):
+    def __init__(self, input_shape: int, num_neurons: int, activation_function=leaky_relu):
         """
         Initialise the Layer. 
         input_shape - the shape of the input that is fed into the NN
-        nr_neurons - nr of neurons (Perceptros) in the NN
-        First (bolean): is this Layer first in NN?
+        num_neurons - number of neurons (Perceptrons) in the layer
         """
-        layer_neurons_list = []
-        for _ in range(nr_neurons):
-            layer_neurons_list.append(Perceptron(input_shape, activation_fun=activation_function))
-        self.layer_neurons_list = layer_neurons_list
-        self.nr_neurons = nr_neurons
+        self.neurons = [
+            Perceptron(input_shape, activation_fun=activation_function)
+            for _ in range(num_neurons)
+        ]
+        self.num_neurons = num_neurons
         self.activation_function = activation_function
-        #an array to store the net sums of perceptrons that are there before being fed into nn
-        self.neuron_nets = []
+        self.neuron_nets = np.zeros(num_neurons)
 
-    def get_weights(self):
+    def get_weights(self, only_weights=False):
         """
-        Returns the weights of the Layer.
-        Dosen't return the bias!
+        Returns the weights of the Layer
+        only_weights (bool): Include the bias term in the returned weights.
         """
-        weights = []
-        for node in self.layer_neurons_list:
-            weights.append(node.weights[:-1])
-        return np.array(weights)
+        if only_weights:
+            return np.array([neuron.weights[:-1] for neuron in self.neurons])
+        return np.array([neuron.weights for neuron in self.neurons])
+    
+    def _update_weights(self, new_weights):
+        """
+        Updates the weights of the layer (includig the bias).
+        new_weights(np.array) - new  weights to be put into the layer
+        """
+        new_weights = np.asarray(new_weights)
+        assert new_weights.shape == (self.num_neurons, self.neurons[0].weights.shape[0]), \
+            f"Expected shape {(self.num_neurons, self.neurons[0].weights.shape[0])}, got {new_weights.shape}"
+        for i, neuron in enumerate(self.neurons):
+            neuron.weights = new_weights[i]
 
-    def _get_net_values(self):
+
+    def _get_net_values(self) -> np.ndarray:
         """
         Returns the net values of every neuron in the layer.
         """
-        nets = []
-        for node in self.layer_neurons_list:
-            nets.append(node.net_perc)
-        return np.array(nets)
+        return np.array([neuron.net_perc for neuron in self.neurons])
         
-    def predict(self, layer_input):
+    def predict(self, layer_input: np.ndarray) -> np.ndarray:
         """
         Produce the output of the NN's layer and store the result.
         """
         self.neuron_nets = []
 
-        results = np.zeros(self.nr_neurons)
-        for i in range(self.nr_neurons):
-            results[i] = self.layer_neurons_list[i].predict(layer_input)
-            self.neuron_nets.append(self.layer_neurons_list[i].net_perc)
+        results = np.zeros(self.num_neurons)
+        for i in range(self.num_neurons):
+            results[i] = self.neurons[i].predict(layer_input)
+            self.neuron_nets.append(self.neurons[i].net_perc)
 
         self.neuron_nets = np.array(self.neuron_nets)
         self.layer_output = results
@@ -113,7 +124,7 @@ class NN:
         #for storing the activations + input (gradient calculation)
         self._a = []
     
-    def add_layer(self, nr_neurons, activation_funct = relu):
+    def add_layer(self, num_neurons, activation_funct = leaky_relu):
         """
         Adds a new layer to the NN.
         Parameters:
@@ -123,14 +134,14 @@ class NN:
         #if list with layers not empty -> take the last outpts!
         if self.nn_layer_list:
             #save the output size of the last layer and put as input to the new layer
-            next_layer_input_size = self.nn_layer_list[self.layer_number-1].nr_neurons
+            next_layer_input_size = self.nn_layer_list[self.layer_number-1].num_neurons
             self.nn_layer_list.append(Layer(input_shape=next_layer_input_size,
-                                            nr_neurons=nr_neurons,activation_function=activation_funct))
+                                            num_neurons=num_neurons,activation_function=activation_funct))
         else:
             #First layer
             first_layer_input_size = self.nn_input_size
             self.nn_layer_list.append(Layer(input_shape=first_layer_input_size,
-                                            nr_neurons=nr_neurons,activation_function=activation_funct))
+                                            num_neurons=num_neurons,activation_function=activation_funct))
 
         #increment the value of layers stored in NN
         self.layer_number += 1
@@ -191,18 +202,20 @@ class NN:
         sig_i = 0 #for indexing sigma_arr
         #get the sigma values for the rest of layers
         for i in range(self.layer_number - 2, -1, -1):
+            print("-------------")
+            print("i (layer from last to first)", i)
             current_layer = self.nn_layer_list[i]
             f = current_layer.activation_function #get the act fun from this layer
             #layer right to current layer 
             layer_right = self.nn_layer_list[i+1]
-            W = layer_right.get_weights()
+            W = layer_right.get_weights(only_weights=True)
+            print("W:\n",W)
             #get the sigma from the right layer
             sig_old = sigma_arr[sig_i]
+            print("sig old: \n", sig_old)
             #derivative
             f_prim = f(current_layer._get_net_values(), derivative=True)
             #calculate the sigma for current layer
-            print("W:\n,", W)
-            print(sig_old)
             sigma = f_prim * (W.T @ sig_old)
             sigma_arr.append(sigma)
             sig_i+=1
@@ -233,9 +246,9 @@ class NN:
         new_weights_of_last_layer = []
         
         #get the derivatives for the weights for every neuron in the lasy layer
-        for i in range(last_layer.nr_neurons):
+        for i in range(last_layer.num_neurons):
             sigma_i = sigma[i]
-            neuron_i = last_layer.layer_neurons_list[i]
+            neuron_i = last_layer.neurons[i]
             #depending on how many weights u have - this big this array will be
             grad_i = sigma_i*prev_layer_output
             #update the weights
@@ -251,29 +264,20 @@ class NN:
             new_weights_of_last_layer.append(np.concatenate([new_weights, [new_bias]]))
 
 
-        
-        #NOW HIDDEN LAYERS + Last Layer for comparasion
-        #array for storing gradients with respect to every layer
-        gradients_w = [] #first element contains gradient for last weights (in the right)
-       
-        print("sigma_arr")
-        print(sigma_arr)
-        #grad_wl = delta_l*a_l-1.T
-        print("a_l")
-        print(self._a)
-
+        #go trough every node
         for l_ix in range(len(sigma_arr)):
-            grad = np.outer(sigma_arr[l_ix], self._a[l_ix])
-            gradients_w.append(grad)
-        
-        print("Gradients")
-        print(gradients_w)
+            #gradient for weights and biases
+            grad_w = np.outer(sigma_arr[l_ix], self._a[l_ix])
+            grad_b = sigma_arr[l_ix]
+            #total gradient
+            gradient = np.column_stack((grad_w, grad_b))
 
+            #Now u can update every layer's weights!
+            layer = self.nn_layer_list[l_ix]
+            old_weights = layer.get_weights()
+            new_weights = old_weights - lbd*gradient
+            layer._update_weights(new_weights)
 
-
-
-      
-            
         return ("Ala")
 
     
